6541aba4e225e74e85b7bc9a5105d6ad7c755418
Muhammad Riziq Zhapran <134038828+nicwaproject@users.noreply.github.com>
2024-10-28 12:41:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,51 @@ function getGuestName() {
 
 // Function to display guest name on the cover
 document.addEventListener('DOMContentLoaded', () => {
-    const guestName = getGuestName();
-    document.getElementById('guest-name').textContent = guestName;
+  const guestName = getGuestName();
+  document.getElementById('guest-name').textContent = guestName;
 
-    // Handle button click to open invitation
-    document.getElementById('openInvitationBtn').addEventListener('click', () => {
-        // Hide the cover
-        document.getElementById('cover').style.display = 'none';
-        // Show navigation and section 1
-        document.getElementById('navigation').style.display = 'block';
-        showSection('section1');
-    });
+  // Handle button click to open invitation
+  document.getElementById('openInvitationBtn').addEventListener('click', () => {
+    // Hide the cover
+    document.getElementById('cover').style.display = 'none';
+    // Show navigation and section 1
+    document.getElementById('navigation').style.display = 'block';
+    showSection('section1');
+    // Music
+    const audio = document.getElementById('weddingMusic');
+    audio.play(); // Mainkan musik
 
-    // Handle navigation buttons
-    const navButtons = document.querySelectorAll('#navigation a');
-    navButtons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            const sectionToShow = button.getAttribute('data-section');
-            showSection(sectionToShow);
-        });
-    });
+    document.getElementById('playPauseButton').style.display = 'block';
+  });
+
+  // Menangani tombol Play/Pause
+  document.getElementById('playPauseButton').addEventListener('click', function() {
+    const audio = document.getElementById('weddingMusic');
+    const icon = document.getElementById('playPauseIcon');
+    
+    if (audio.paused) {
+      audio.play();
+      icon.classList.remove('fa-play');
+      icon.classList.add('fa-pause');
+    } else {
+      audio.pause();
+      icon.classList.remove('fa-pause');
+      icon.classList.add('fa-play');
+    }
+  });
+
+  // Handle navigation buttons
+  const navButtons = document.querySelectorAll('#navigation a');
+  navButtons.forEach(button => {
+      button.addEventListener('click', (e) => {
+          e.preventDefault();
+          const sectionToShow = button.getAttribute('data-section');
+          showSection(sectionToShow);
+      });
+  });
 
 
+  //HANDLE COUNTDOWN
     const countdown = () => {
       const weddingDate = new Date("2024-12-31T00:00:00").getTime(); // Set tanggal pernikahan
       const now = new Date().getTime();
@@ -56,66 +78,126 @@ document.addEventListener('DOMContentLoaded', () => {
     // Perbarui countdown setiap detik
     const timer = setInterval(countdown, 1000);
 
-     // Mengambil data RSVP ketika halaman dimuat
-fetch('https://tema-4-rsvp-handle.glitch.me/rsvps')
-.then(response => response.json())
-.then(data => {
-  data.forEach(item => {
-    const messageItem = document.createElement('div');
-    messageItem.classList.add('message-item');
-    messageItem.innerHTML = `
-      <h4>${item.name} (${item.attendance})</h4>
-      <p>${item.message}</p>
-    `;
-    document.getElementById('messageList').appendChild(messageItem);
-  });
-})
-.catch(error => console.error('Error fetching RSVP data:', error));
+  //HANDLE PESAN
+  // Mengambil data RSVP ketika halaman dimuat
+  fetch('https://tema-4-rsvp-handle.glitch.me/rsvps')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+      const messageItem = document.createElement('div');
+      messageItem.classList.add('message-item');
+      messageItem.innerHTML = `
+        <h4>${item.name} (${item.attendance})</h4>
+        <p>${item.message}</p>
+      `;
+      document.getElementById('messageList').appendChild(messageItem);
+    });
+  })
+  .catch(error => console.error('Error fetching RSVP data:', error));                             
 
-// Menangani form RSVP
-document.getElementById('rsvpForm').addEventListener('submit', function (event) {
-event.preventDefault(); // Prevent default form submission
+  // Menangani form RSVP
+  document.getElementById('rsvpForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent default form submission
 
-// Get form data
-const guestNameRSVP = document.getElementById('guestNameRSVP').value.trim(); // Ganti ID
-const guestMessage = document.getElementById('guestMessage').value.trim();
-const attendance = document.getElementById('attendance').value;
+  // Get form data
+  const guestNameRSVP = document.getElementById('guestNameRSVP').value.trim(); // Ganti ID
+  const guestMessage = document.getElementById('guestMessage').value.trim();
+  const attendance = document.getElementById('attendance').value;
 
-// Check if all required fields are filled
-if (!guestNameRSVP || !guestMessage || !attendance) {
-  alert('Please fill out all fields.');
-  return;
-}
-
-// Create an object with form data
-const formData = {
-  name: guestNameRSVP,
-  message: guestMessage,
-  attendance: attendance
-};
-
-// Submit form data
-fetch('https://tema-4-rsvp-handle.glitch.me/rsvp', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(formData)
-})
-.then(response => response.json())
-.then(data => {
-  if (data.success) {
-    alert('RSVP submitted successfully!');
-    location.reload(); // Optionally, reload the page after submission
-  } else {
-    alert('Error: ' + data.error);
+  // Check if all required fields are filled
+  if (!guestNameRSVP || !guestMessage || !attendance) {
+    alert('Please fill out all fields.');
+    return;
   }
-})
-.catch(error => {
-  console.error('Error submitting RSVP:', error);
+
+  // Create an object with form data
+  const formData = {
+    name: guestNameRSVP,
+    message: guestMessage,
+    attendance: attendance
+  };
+
+  // Submit form data
+  fetch('https://tema-4-rsvp-handle.glitch.me/rsvp', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      alert('RSVP submitted successfully!');
+      location.reload(); // Optionally, reload the page after submission
+    } else {
+      alert('Error: ' + data.error);
+    }
+  })
+  .catch(error => {
+    console.error('Error submitting RSVP:', error);
+  });
+  }); 
+
+
+  // GALLERY HANDLE
+  const galleryItems = document.querySelectorAll(".gallery-item");
+    const lightbox = document.querySelector(".lightbox");
+    const lightboxImage = document.querySelector(".lightbox-image");
+    const closeBtn = document.querySelector(".lightbox .close");
+    const prevBtn = document.querySelector(".lightbox .prev");
+    const nextBtn = document.querySelector(".lightbox .next");
+
+    let currentIndex = 0;
+
+    function openLightbox(index) {
+        lightbox.style.display = "flex";
+        currentIndex = index;
+        lightboxImage.src = galleryItems[currentIndex].getAttribute("data-src");
+    }
+
+    function closeLightbox() {
+        lightbox.style.display = "none";
+    }
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % galleryItems.length;
+        lightboxImage.src = galleryItems[currentIndex].getAttribute("data-src");
+    }
+
+    function showPrevImage() {
+        currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+        lightboxImage.src = galleryItems[currentIndex].getAttribute("data-src");
+    }
+
+    galleryItems.forEach((item, index) => {
+        item.addEventListener("click", () => openLightbox(index));
+    });
+
+    closeBtn.addEventListener("click", closeLightbox);
+    nextBtn.addEventListener("click", showNextImage);
+    prevBtn.addEventListener("click", showPrevImage);
 });
-});
-});
+
+  // GIFT HANDLE
+   function openBankModal(bankName, accountNumber) {
+     document.getElementById('bankModal').style.display = 'flex';
+     document.getElementById('bankName').innerText = bankName;
+     document.getElementById('bankAccount').innerText = accountNumber;
+ }
+ 
+ function closeBankModal() {
+     document.getElementById('bankModal').style.display = 'none';
+ }
+ 
+ function copyAccountNumber() {
+     const accountNumber = document.getElementById('bankAccount').innerText;
+     navigator.clipboard.writeText(accountNumber).then(() => {
+         alert("Nomor rekening berhasil disalin!");
+     }, () => {
+         alert("Gagal menyalin nomor rekening.");
+     });
+ }  
 
 // Function to show a specific section within the frame
 function showSection(sectionId) {
